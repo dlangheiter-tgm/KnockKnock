b8f2c83d7f23a729d1844350113708d6ac511bdc
David Langheiter <david@langheiter.com>
2018-11-24 14:05:09 +0100

    Moved to executor service

--- src/main/java/server/KKServer.java
+++ src/main/java/server/KKServer.java
@@ -2,8 +2,31 @@ package server;
 
 import java.net.*;
 import java.io.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class KKServer {
+
+    KKServer(int portNumber) {
+        boolean listening = true;
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+
+        try (
+                ServerSocket serverSocket = new ServerSocket(portNumber);
+        ) {
+            System.out.println("Server started on localhost:" + portNumber);
+
+            while(listening) {
+                pool.execute(new KKThread(serverSocket.accept()));
+            }
+
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                    + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static void main(String[] args) throws IOException {
 
         if (args.length != 1) {
@@ -12,19 +35,6 @@ public class KKServer {
         }
 
         int portNumber = Integer.parseInt(args[0]);
-
-        try (
-                ServerSocket serverSocket = new ServerSocket(portNumber);
-        ) {
-
-            while(true) {
-                new KKThread(serverSocket.accept()).start();
-            }
-
-        } catch (IOException e) {
-            System.out.println("Exception caught when trying to listen on port "
-                    + portNumber + " or listening for a connection");
-            System.out.println(e.getMessage());
-        }
+        new KKServer(portNumber);
     }
 }
